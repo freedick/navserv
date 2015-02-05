e5a108277be635169d39f2ae1cc5232c82bfb0ab
Andreas Heffsten <jofen@A1203-06-sm.lab.ltu.se>
2015-02-05 17:24:28 +0100

    Added fuctions whit pre-written sql commands in the database class

--- util/dbc.py
+++ util/dbc.py
@@ -10,4 +10,23 @@ class DBC:
 	def query(self, query):
 		cur = self.db.cursor() 
 		cur.execute(query)
-		return cur.fetchall()
+		return cur.fetchall(
+
+
+
+	def addPoint(self,lat,lon):
+		return self.query("INSERT INTO RoadPoints (Lat, Lon) VALUES (lat, lon)")
+
+	def addSegment(self,pointa,pointb):
+		temppointa = self.addPoint(pointa.Lat,pointa.Lon)
+		temppontb = self.addpoint(pointb.Lat,pointb.Lon)
+		return self.query("INSERT INTO RoadSegments (PointA, PointB, Status) VALUES (temppointa.ID, temppointb.ID, 0)")
+
+
+	def addSegmentToRoute(self,routeid, segmentid):
+		return self.query("INSERT INTO RouteSegments (RouteID,RoadSegemntID) VALUES (routeid, segmentid)")
+
+
+	def addRoute(self,name):
+		return self.query("INSERT INTO Routes (Name, Version) VALUES (name, 0)")
+		
