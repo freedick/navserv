ee6fcae0861833321041d53a0a201c9adcab8261
navigator++(+) <navigator@A1203-06-sm.lab.ltu.se>
2015-02-07 14:03:58 +0100

    Modular dbc instead of classed dbc

--- util/dbc.py
+++ util/dbc.py
@@ -1,32 +1,31 @@
 import MySQLdb
+__DATABASE__ = None
 
-class DBC:
-	def __init__(self, host, username, password, dbname):
-		self.db = MySQLdb.connect(
-			host="130.240.5.34",
-			user=username,
-			passwd=password,
-			db=dbname)
-	def query(self, query):
-		cur = self.db.cursor() 
-		cur.execute(query)
-		return cur.fetchall(
+def connect(host, username, password, dbname):
+	global __DATABASE__
+	__DATABASE__ = MySQLdb.connect(
+		host=host,
+		user=username,
+		passwd=password,
+		db=dbname)
 
+def query(query):
+	global __DATABASE__
+	if query[-1] != ";":
+		query=query+";"
+	cur = __DATABASE__.cursor() 
+	cur.execute(query)
+	return cur.fetchall()
 
+def addPoint(lat,lon):
+	return query("INSERT INTO RoadPoints (Lat, Lon) VALUES (lat, lon)")
 
-	def addPoint(self,lat,lon):
-		return self.query("INSERT INTO RoadPoints (Lat, Lon) VALUES (lat, lon)")
+def addSegment(pointa,pointb):
+	return query("INSERT INTO RoadSegments (PointA, PointB, Status) VALUES (%d, %d, 0)" % (pointa.id,pointb.id))
 
-	def addSegment(self,pointa,pointb):
-		temppointa = self.addPoint(pointa.Lat,pointa.Lon)
-		temppontb = self.addpoint(pointb.Lat,pointb.Lon)
-		return self.query("INSERT INTO RoadSegments (PointA, PointB, Status) VALUES (temppointa.ID, temppointb.ID, 0)")
+def addSegmentToRoute(routeid, segmentid):
+	return query("INSERT INTO RouteSegments (RouteID,RoadSegemntID) VALUES (routeid, segmentid)")
 
+def addRoute(name):
+	return query("INSERT INTO Routes (Name, Version) VALUES (name, 0)")
 
-	def addSegmentToRoute(self,routeid, segmentid):
-		return self.query("INSERT INTO RouteSegments (RouteID,RoadSegemntID) VALUES (routeid, segmentid)")
-
-
-	def addRoute(self,name):
-		return self.query("INSERT INTO Routes (Name, Version) VALUES (name, 0)")
-		
